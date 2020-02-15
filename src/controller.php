f367c249639dfffd6f879753aba73ac0c84c33b5
chenghao xiong <chenghao.xiong@gmail.com>
2020-02-14 23:03:03 -0700

    Test

--- src/controller.php
+++ src/controller.php
@@ -3,5 +3,5 @@
 include "DatabaseAdapter.php";
 $theDB = new DatabaseAdaptor();
 $sub_str = $_GET["value"];
-echo json_encode($theDB ->getValue($sub_str));
-?>
+echo json_encode($theDB ->getValue($sub_str)); 
+?>  
