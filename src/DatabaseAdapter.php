74d1c00c509ea12f6eaf73c43d95245bc4feb9dc
chenghao xiong <chenghao.xiong@gmail.com>
2020-02-14 22:44:02 -0700

    Test

---
+++ src/DatabaseAdapter.php
@@ -0,0 +1,29 @@
+<?php
+//Felicity Aldava
+class DatabaseAdaptor {
+    private $DB; // The instance variable used in every method
+    // Connect to an existing data based named 'first'
+    public function __construct() {
+        $dataBase =
+        'mysql:dbname=test;charset=utf8;host=127.0.0.1';
+        $user =
+        'root';
+        $password =
+        ''; // Empty string with XAMPP install
+        try {
+            $this->DB = new PDO ( $dataBase, $user, $password );
+            $this->DB->setAttribute ( PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION );
+        } catch ( PDOException $e ) {
+            echo ('Error establishing Connection');
+            exit ();
+        }
+    } // . . . continued
+    
+    public function getValue($substr){
+        $stmt = $this->DB->prepare("Select * from test where value =".$substr.";");
+        $stmt->execute();
+        return $stmt->fetchAll ( PDO::FETCH_ASSOC );
+    }
+}
+?>
---
+++ src/controller.php
@@ -0,0 +1,7 @@
+<?php
+//Felicity Aldava
+include "DatabaseAdapter.php";
+$theDB = new DatabaseAdaptor();
+$sub_str = $_GET["value"];
+echo json_encode($theDB ->getValue($sub_str));
+?>
