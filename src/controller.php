14e77f8828205dc50df3a230faf9043d07e91f02
JiazhengXiong <jiazhengxiong@dhcp-10-134-200-159.uawifi.arizona.edu>
2020-02-21 14:12:19 -0700

    change GET TO POST

--- src/controller.php
+++ src/controller.php
@@ -3,18 +3,18 @@
 include "DatabaseAdapter.php";
 $theDB = new DatabaseAdaptor();
 
-$loginUsername = $_GET["account"];
-$loginPwd = $_GET["pwd"];
+$loginUsername = $_POST["account"];
+$loginPwd = $_POST["pwd"];
 echo json_encode($theDB ->userLogin($loginUsername, $loginPwd));
 
 
 
-$firstName = $_GET["firstName"];
-$lastName = $_GET["lastName"];
-$email = $_GET["email"];
-$phone = $_GET["phone"];
-$userName = $_GET["userName"];
-$pwd = $_GET["pwd"];
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$userName = $_POST["userName"];
+$pwd = $_POST["pwd"];
 echo json_encode($theDB ->insertUser($first_name,$last_name,
     $phone, $email, $username, $pwd));
 
